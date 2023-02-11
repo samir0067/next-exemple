13ad0511e1a07b644fabfd5910a117cd9e0f1e97
samir0067 <arrahmani.samir@gmail.com>
2023-02-11 22:22:52 +0100

    add content items.tsx

--- pages/blog/items.tsx
+++ pages/blog/items.tsx
@@ -9,7 +9,23 @@ const Items = () => {
         <title>Items</title>
       </Head>
       <Layout>
-        <h1>Items</h1>
+        <h1>Bienvenue sur notre site web</h1>
+        <p>{"Nous sommes heureux de vous accueillir sur notre site web dédié à l'information et à l'entrainement."}</p>
+        <ul>
+          <li>Actualités</li>
+          <li>Sport</li>
+          <li>Culture</li>
+          <li>Voyage</li>
+        </ul>
+        <ol>
+          <li>Introduction</li>
+          <li>Notre histoire</li>
+          <li>Notre mission</li>
+          <li>Nos valeurs</li>
+        </ol>
+        <blockquote>
+          {"Le savoir est la clé du succès."}
+        </blockquote>
       </Layout>
     </>
 
